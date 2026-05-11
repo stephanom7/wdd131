2c12bcb170047365d19be912f90bdf37380c226e
stephanom7 <smartinez6@byupathway.edu>
2026-05-11 13:13:00 -0500

    week1

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Insert current year dynamically
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Insert last modified date dynamically
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
